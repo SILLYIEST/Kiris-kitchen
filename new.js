500be821e90c75e399846925a3c73e161609861e
silly! <s1874530@online.houstonisd.org>
2024-10-22 18:08:29 +0000

    trying to fix my life

--- new.js
+++ new.js
@@ -1,27 +1,11 @@
-var carousel = $(".carousel"),
-    items = $(".item"),
-    currdeg  = 0;
-
-$(".next").on("click", { d: "n" }, rotate);
-$(".prev").on("click", { d: "p" }, rotate);
-
-function rotate(e){
-  if(e.data.d=="n"){
-    currdeg = currdeg - 60;
-  }
-  if(e.data.d=="p"){
-    currdeg = currdeg + 60;
-  }
-  carousel.css({
-    "-webkit-transform": "rotateY("+currdeg+"deg)",
-    "-moz-transform": "rotateY("+currdeg+"deg)",
-    "-o-transform": "rotateY("+currdeg+"deg)",
-    "transform": "rotateY("+currdeg+"deg)"
-  });
-    items.css({
-    "-webkit-transform": "rotateY("+(-currdeg)+"deg)",
-    "-moz-transform": "rotateY("+(-currdeg)+"deg)",
-    "-o-transform": "rotateY("+(-currdeg)+"deg)",
-    "transform": "rotateY("+(-currdeg)+"deg)"
-  });
-}
+$(".slider").jasCarousel({
+  margin: 20,
+  auto: false,
+  speed: 800,
+  delay: 2000,
+  slideFrontFace: false,
+  prevText:'Prev',
+  nextText:'Next',
+  slideFrontFace:true,
+  moveOnSlideClick:true
+})
